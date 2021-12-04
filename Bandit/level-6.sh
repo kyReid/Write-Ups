ab6b62f1b576a9fde8bc9b7b73d604e1bea8832b
Kyle Reid <kyreid14@gmail.com>
2021-12-04 14:19:41 -0500

    Update level-6.sh

--- Bandit/level-6.sh
+++ Bandit/level-6.sh
@@ -1,3 +1,11 @@
+"
+The password for the next level is stored in a file somewhere under the inhere directory and has all of the following properties:
+
+human-readable
+1033 bytes in size
+not executable
+"
+
 # find files of size 1033 bytes 
 # is human readable
 # not executable
